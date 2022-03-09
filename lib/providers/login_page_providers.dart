1d228e914064bea08bf2f1fc2094eeed4727b524
VIP-Dev-Invisible <97518797+VIP-Dev-Invisible@users.noreply.github.com>
2022-03-09 21:37:51 +0545

    Update login_page_providers.dart

--- lib/providers/login_page_providers.dart
+++ lib/providers/login_page_providers.dart
@@ -1,10 +1,11 @@
-// ignore: deprecated_member_use
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final emailProvider = StateProvider<String>((ref) => '');
+//final emailProvider = StateProvider<String>((ref) => '');
+final emailProvider = StateProvider<String?>((ref) {
+  return null;
+});
 
-final passwordProvider = StateProvider<String>((ref) => '');
-// final passwordProvider = StateProvider<String>((ref) {
-//   return '';
-// });
+//final passwordProvider = StateProvider<String>((ref) => '');
+final passwordProvider = StateProvider<String?>((ref) {
+  return null;
+});
